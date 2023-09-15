2bf5d55bbfe721e4962eaa4275be30be514d0a66
Yulyano Thomas Djaya <ydjaya@pajak.io>
2023-09-15 10:08:35 +0700

    Init

---
+++ adhoc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ansible all -m yum_repository -a  'name=EPEL description=RHEL8 baseurl=https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm gpgcheck=no enabled=yes' -b --ask-become-pass
+
